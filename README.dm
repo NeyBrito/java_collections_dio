fd147f698973f918a04b44a2775840a311baefdd
Ney Brito <ney.brito@etec.sp.gov.br>
2021-06-21 17:49:01 -0300

    Signed-off-by: Ney Brito <ney.brito@etec.sp.gov.br>

---
+++ README.dm
@@ -0,0 +1,40 @@
+# Curso Introdutório Collections Java
+
+Resolução dos exercícios propostos: **CURSO INTRODUTÓRIO COLLECTIONS FRAMEWORK JAVA**. Curso este que tive a honra de co-criar com a plataforma de cursos online **[DIGITAL INNOVATION ONE ](https://web.digitalinnovation.one/home)**🧡💛 e disponibilizado de forma gratuita para a comunidade dos desenvolvedores Java.
+
+
+
+## 🛑 Pré-requistos
+
+-  Java JDK 11
+-  IDE IntelliJ
+-  Conhecer a sintaxe Java
+-  Noção acerca do Paradigma de Programação Orientada a Objeto
+-  Disposição para estudar
+
+## 📚 Ementa
+
+🔸 **Visão geral: Java Collections Framework e hierarquia**
+
+🔸 **List**
+Coleção com iterações ordenadas: List
+Conhecendo os métodos
+Ordenação da coleção List
+
+🔸 **Set**
+Coleção com singularidade: Set
+Conhecendo os métodos
+Ordenação da coleção Set
+
+🔸 **Map**
+Coleção de pares: Map
+Conhecendo os métodos
+Ordenação de Map
+
+🔸 **Stream API**
+Visão geral: Classe Anônima - Functional Interface - Lambda - Method Reference
+Stream API
+Conhecendo as operações
+
+------
+
---
+++ java_collection_dio/src/main/java/ExemploList.java
@@ -0,0 +1,104 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+class ExemploList {
+    public static void main(String[] args) {
+
+        //List<Double> notas = new ArrayList<Double>();
+
+        //ArrayList<Double> notas = new ArrayList<>(); //Não recomendado (programar para interface não para implementação)
+
+        //List<Double> notas = new ArrayList<>(Array.asList(7d, 8.5,9.3,5d,0d, 3.6));
+
+        //List<Double> notas = ArrayList<>(7d, 8.5,9.3,5d,0d, 3.6); //Não pode adcionar nem tirar elementos da lista
+        //notas.add(10d);
+        //System.out.println(notas);
+
+        //List<Double> notas = List.of(7d, 8.5,9.3,5d,0d, 3.6); //Não pode adcionar nem tirar elementos da lista
+        //notas.add(1d);
+        //notas.remove(5d);0
+        //System.out.println(notas);
+
+        System.out.println("Crie uma lista e adcione as sete notas.");
+        ArrayList<Double> notas = new ArrayList<Double>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
+
+        System.out.println("Adcione na lista a nota 8.0 na posição 4: ");
+        notas.add(4,8d);
+        System.out.println(notas);
+
+        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+        notas.set(notas.indexOf(5d), 6.0);
+        System.out.println(notas);
+
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+
+//      System.out.println("Exiba as notas na ordem que foram infomados: ");
+//      for (Double nota: notas) System.out.println(nota);
+
+        System.out.println("Exiba a teceira nota adcionada: " + notas.get(2));
+        System.out.println((notas.toString()));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+        System.out.println("Exiba a menor nota: " + Collections.max(notas));
+
+        System.out.println("Exiba a soma dos valores " );
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){     //Verifica se há elementos na proxima posição da lista
+            Double next = iterator.next(); //Põe o proximo elemento dentro da variável next
+            soma += next; //Atribui e soma o valor de next na variável soma (assim vai somando e atribuindo)
+        }
+        System.out.println("A soma é: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0.0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota 0.0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        Double menor = 0d;
+        while (iterator1.hasNext()){
+            double next = iterator1.next();
+            if (next < 7d) {
+                iterator1.remove();
+            }
+            System.out.println(notas);
+        }
+
+/*        System.out.println("Apague toda a lista");
+        notas.clear();
+        System.out.println(notas);*/
+
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
+
+
+
+
+
+
+
+
+
+
+    }
+}
